8130bea3aa3e52b0ecf677427b8c68a7dcc7f975
Shakeel Haider <shkhaider2015@gmail.com>
2023-03-13 00:45:19 +0500

    Added poetry genrator file

---
+++ class_4_practice.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+
+model = tf.keras.models.load_model('poetry-generator.h5')
+
+seed_text = "I've got a bad feeling about this"
+next_words = 100
+max_sequence_len=0
+
+tokenizer = Tokenizer()
+
+for _ in range(next_words):
+    token_list = tokenizer.texts_to_sequences([seed_text])[0]
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+    predicted = np.argmax(model.predict(token_list), axis=-1)
+    output_word = ""
+    for word, index in tokenizer.word_index.items():
+        if index == predicted:
+            output_word = word
+            break
+    seed_text += " " + output_word
+print(seed_text)
